4c68aa7bebeda26ae38df73286f6ac880bb692b0
Eric Mai <ericmai10314@gmail.com>
2022-05-18 01:03:18 -0400

    studentv

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,17 +4,73 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
-const StudentView = (props) => {
-  const { student } = props;
+import { Link } from "react-router-dom";
 
-  // Render a single Student view 
+const StudentView = (props) => {
+  const { student, removeStudent } = props;
+  if (!student.campus) {
+    return (
+      <div>
+        <h1>{student.firstname + " " + student.lastname}</h1>
+        <p>
+          <span style={{ fontWeight: "500" }}>First Name: </span>
+          {student.firstname}
+        </p>
+        <p>
+          <span style={{ fontWeight: "500" }}>Last Name: </span>
+          {student.lastname}
+        </p>
+        <p>
+          <span style={{ fontWeight: "500" }}>Email: </span>
+          {student.email}
+        </p>
+        <p>
+          <span style={{ fontWeight: "500" }}>GPA: </span>
+          {student.gpa}
+        </p>
+
+        <p>(Not Enrolled Currently)</p>
+        <Link to={`/editstudent/${student.id}`}>
+          <button>Edit</button>
+        </Link>
+        <button onClick={() => removeStudent(student.id)}>Delete</button>
+      </div>
+    );
+  }
+
+  // Render a single Student view
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
-      <h3>{student.campus.name}</h3>
+      <p>
+        <span style={{ fontWeight: "500" }}>First Name: </span>
+        {student.firstname}
+      </p>
+      <p>
+        <span style={{ fontWeight: "500" }}>Last Name: </span>
+        {student.lastname}
+      </p>
+      <p>
+        <span style={{ fontWeight: "500" }}>Email: </span>
+        {student.email}
+      </p>
+      <p>
+        <span style={{ fontWeight: "500" }}>GPA: </span>
+        {student.gpa}
+      </p>
+      <p>
+        <span style={{ fontWeight: "500" }}>Enrolled in</span>
+        <br />
+        <Link to={`/campus/${student.campus.id}`}>
+          <h5>{student.campus.name}</h5>
+        </Link>
+      </p>
+      <Link to={`/editstudent/${student.id}`}>
+        <button>Edit</button>
+      </Link>
+      <button onClick={() => removeStudent(student.id)}>Delete</button>
     </div>
   );
-
 };
 
 export default StudentView;
